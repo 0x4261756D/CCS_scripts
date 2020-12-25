e209d5871411449163b1d3cfaf113e612e07fee9
Rundas <38040866+Rundas01@users.noreply.github.com>
2020-12-25 20:29:44 +0100

    "Spider Dragon" und "Pepsiman" scripts aktualisiert

--- script/INSU_Extradeck_script/c21148163.lua
+++ script/INSU_Extradeck_script/c21148163.lua
@@ -1,25 +1,16 @@
 --Spider Dragon, Nightmare Creator
-function c21148163.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,nil,4,4,c21148163.lcheck)
-	--add
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND)
-	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetCondition(c21148163.thcon)
-	e1:SetTarget(c21148163.thtg)
-	e1:SetOperation(c21148163.thop)
-	c:RegisterEffect(e1)
+	Link.AddProcedure(c,aux.NOT(aux.FilterBoolFunctionEx(Card.IsType,TYPE_TOKEN)),4,4,s.lcheck)
 	--Disable
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_DISABLE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(c21148163.tgtg)
+	e2:SetTarget(s.tgtg)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_DISABLE_EFFECT)
@@ -30,15 +21,15 @@ function c21148163.initial_effect(c)
 	e4:SetCode(EVENT_FREE_CHAIN)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(2)
-	e4:SetCondition(c21148163.seqcon)
-	e4:SetOperation(c21148163.seqop)
+	e4:SetCondition(s.seqcon)
+	e4:SetOperation(s.seqop)
 	c:RegisterEffect(e4)
 	--spsummon only
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_FIELD)
 	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e5:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e5:SetValue(c21148163.splimit)
+	e5:SetValue(s.splimit)
 	c:RegisterEffect(e5)
 	--cannot release
 	local e6=Effect.CreateEffect(c)
@@ -52,11 +43,11 @@ function c21148163.initial_effect(c)
 	e7:SetCode(EFFECT_UNRELEASABLE_NONSUM)
 	c:RegisterEffect(e7)
 end
-function c21148163.cfilter(c,tp,seq,lc)
+function s.cfilter(c,tp,seq,lc)
 	return c:GetSequence()==seq and c:IsControler(tp)
 	 	and c:IsCanBeLinkMaterial(lc,tp)
 end
-function c21148163.lcheck(g,lc,tp)
+function s.lcheck(g,lc,tp)
 	local emz1 = Duel.GetFieldCard(tp,LOCATION_MZONE,5)
     local emz2 = Duel.GetFieldCard(tp,LOCATION_MZONE,6)
     if emz1 or emz2 then
@@ -68,19 +59,19 @@ function c21148163.lcheck(g,lc,tp)
     return g:IsExists(Card.IsType,1,nil,TYPE_EFFECT,lc,SUMMON_TYPE_LINK,tp)
 end
 
-function c21148163.splimit(e,se,sp,st)
+function s.splimit(e,se,sp,st)
 	local c=e:GetHandler()
 	return c:IsLocation(LOCATION_EXTRA)
 end
 
-function c21148163.thcon(e,tp,eg,ep,ev,re,r,rp)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
-function c21148163.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c21148163.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(Card.IsAbleToHand),tp,LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
@@ -89,23 +80,23 @@ function c21148163.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function c21148163.tgtg(e,c)
+function s.tgtg(e,c)
 	local tp=e:GetHandler():GetControler()
 	return e:GetHandler():GetLinkedGroup():IsContains(c)
 		and not (c:IsSummonType(SUMMON_TYPE_SPECIAL) and c:IsControler(tp))
 end
 
-function c21148163.seqcon(e,tp,eg,ep,ev,re,r,rp)
+function s.seqcon(e,tp,eg,ep,ev,re,r,rp)
 	local seq=e:GetHandler():GetSequence()
 	if seq<5 then return false end
 	return not e:GetHandler():IsStatus(STATUS_CHAINING)
 end
-function c21148163.rmfilter(c,tp,e,a)
+function s.rmfilter(c,tp,e,a)
 	return ((c:GetSequence()==e:GetHandler():GetSequence()+a and c:IsControler(tp))
 	 or (c:GetSequence()==e:GetHandler():GetSequence() and c:IsControler(1-tp)))
 		and c:IsAbleToRemove()
 end
-function c21148163.seqop(e,tp,eg,ep,ev,re,r,rp)
+function s.seqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or c:IsControler(1-tp) then return end
 	local seq=c:GetSequence()
@@ -115,8 +106,8 @@ function c21148163.seqop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.CheckLocation(tp,LOCATION_MZONE,seq+a) then
 		Duel.MoveSequence(c,seq+a)
 	else
-		if Duel.IsExistingMatchingCard(c21148163.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp,e,a) then
-			local g=Duel.SelectMatchingCard(tp,c21148163.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,tp,e,a)
+		if Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp,e,a) then
+			local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,tp,e,a)
 			Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 			Duel.MoveSequence(c,seq+a)
 		end
--- script/INSU_Maindeck_script/c92899881.lua
+++ script/INSU_Maindeck_script/c92899881.lua
@@ -1,75 +1,81 @@
 --Pepsiman
-function c92899881.initial_effect(c)
-	--search
+local s,id=GetID()
+function s.initial_effect(c)
+	--search on summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(92899881,0))
-	e1:SetCountLimit(1)
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e1:SetTarget(c92899881.thtg1)
-	e1:SetOperation(c92899881.thop1)
+	e1:SetTarget(s.thtg1)
+	e1:SetOperation(s.thop1)
 	c:RegisterEffect(e1)
 	--change ATK
 	local e2=Effect.CreateEffect(c)
-	e2:SetCountLimit(1,92899881)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_SET_ATTACK)
+	e2:SetCode(EFFECT_SET_ATTACK_FINAL)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e2:SetValue(3000)
-	e2:SetCondition(c92899881.con)
+	e2:SetCondition(s.con)
 	c:RegisterEffect(e2)
-	--tribute
-local e3=Effect.CreateEffect(c)
+	--search on going to GY
+	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e3:SetCountLimit(1,92899881)
-	e3:SetCondition(c92899881.thcon2)
-	e3:SetTarget(c92899881.thtg2)
-	e3:SetOperation(c92899881.thop2)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(s.thcon2)
+	e3:SetTarget(s.thtg2)
+	e3:SetOperation(s.thop2)
 	c:RegisterEffect(e3)
+	--no tribute if cola is on field
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_SUMMON_PROC)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetCondition(s.con)
+	c:RegisterEffect(e4)
 end
-function c92899881.cfilter(c)
+function s.cfilter(c)
 	return c:IsFaceup() and c:IsCode(92899882)
 end
-function c92899881.con(e)
-	local c=e:GetHandler()
-	return Duel.IsExistingMatchingCard(c92899881.cfilter,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,1,c)
+function s.con(e,tp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
-function c92899881.thfilter1(c)
+function s.thfilter1(c)
 	return c:IsCode(52894680) and c:IsAbleToHand()
 end
-function c92899881.thtg1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c92899881.thfilter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+function s.thtg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-function c92899881.thop1(e,tp,eg,ep,ev,re,r,rp)
+function s.thop1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c92899881.thfilter1),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter1),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c92899881.thcon2(e,tp,eg,ep,ev,re,r,rp)
+function s.thcon2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return re and re:GetHandler():IsType(TYPE_SPELL) and c:IsReason(REASON_EFFECT) --and c:IsPreviousLocation(LOCATION_MZONE)
 end
-function c92899881.thfilter2(c)
+function s.thfilter2(c)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
 end
-function c92899881.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c92899881.thfilter2,tp,LOCATION_GRAVE,0,1,nil) end
+function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
-function c92899881.thop2(e,tp,eg,ep,ev,re,r,rp)
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c92899881.thfilter2),tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter2),tp,LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)

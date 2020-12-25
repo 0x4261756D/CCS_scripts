be7aa46dd7428b1413d9256a781d85286df2d471
Rundas <38040866+Rundas01@users.noreply.github.com>
2020-12-25 17:15:52 +0100

    "Meteorite Kicking Dinosaur" script aktualisiert

--- script/INSU_Extradeck_script/c26694382.lua
+++ script/INSU_Extradeck_script/c26694382.lua
@@ -1,17 +1,17 @@
 --Meteorite Kicking Dinosaur
-function c26694382.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,c26694382.matfilter,1,1)
+	Link.AddProcedure(c,s.matfilter,2,2,s.matcheck)
 	--destroy
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCountLimit(1,26694382)
-	e1:SetCost(c26694382.cost)
-	e1:SetCondition(c26694382.condition)
-	e1:SetTarget(c26694382.target)
-	e1:SetOperation(c26694382.operation)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	--self destroy
 	local e2=Effect.CreateEffect(c)
@@ -19,9 +19,9 @@ function c26694382.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCondition(c26694382.sdcon)
-	e2:SetTarget(c26694382.destg)
-	e2:SetOperation(c26694382.desop)
+	e2:SetCondition(s.sdcon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_FLIPSUMMON_SUCCESS)
@@ -30,69 +30,48 @@ function c26694382.initial_effect(c)
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e4)
 end
-function c26694382.matfilter(c,lc,sumtype,tp)
+function s.matfilter(c,lc,sumtype,tp)
 	return c:IsLevelBelow(4) and c:IsRace(RACE_DINOSAUR,lc,sumtype,tp)
 end
-function c26694382.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.matcheck(g,lc,sumtype,tp)
+	return g:GetClassCount(Card.GetLevel)==1
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
-function c26694382.rescon(sg,e,tp,mg)
-	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:IsExists(c26694382.costchk1,1,nil,sg)
-end
-function c26694382.costchk1(c,sg)
-	return c:IsRace(RACE_DINOSAUR) and sg:FilterCount(Card.IsAttribute,c,ATTRIBUTE_DARK)==1
-end
-function c26694382.costfilter1(c,race)
-	return c:IsRace(race) and c:IsAbleToRemoveAsCost()
-end
-function c26694382.costfilter2(c,att)
-	return c:IsAttribute(att) and c:IsAbleToRemoveAsCost()
-end
-function c26694382.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	local rg1=Duel.GetMatchingGroup(c26694382.costfilter1,tp,LOCATION_DECK,0,nil,RACE_DINOSAUR)
-	local rg2=Duel.GetMatchingGroup(c26694382.costfilter2,tp,LOCATION_DECK,0,nil,ATTRIBUTE_DARK)
-	local rg=rg1:Clone()
-	rg:Merge(rg2)
-	if rg1:GetCount()>0 and rg2:GetCount()>0 
-		and aux.SelectUnselectGroup(rg,e,tp,2,2,c26694382.rescon,0) then
-		local g=aux.SelectUnselectGroup(rg,e,tp,2,2,c26694382.rescon,1,tp,HINTMSG_REMOVE)
-		Duel.Remove(g,POS_FACEUP,REASON_COST)
-	end
-end
-function c26694382.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_DECK)
 end
-function c26694382.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
+	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_DECK,0,1,1,nil):Filter(Card.IsMonster,nil)
+	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
-function c26694382.sdfilter(c)
+function s.sdfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_DINOSAUR)
 end
-function c26694382.sdcon(e)
-	return Duel.IsExistingMatchingCard(c26694382.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
+function s.sdcon(e)
+	return Duel.IsExistingMatchingCard(s.sdfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
 end
-function c26694382.desfilter(c)
+function s.desfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsDestructable()
 end
-function c26694382.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c26694382.desfilter,tp,LOCATION_DECK,0,1,nil)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_DECK,0,1,nil)
 		and e:GetHandler():IsDestructable() end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,tp,LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_DECK)
 end
-function c26694382.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,c26694382.desfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
+	local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end

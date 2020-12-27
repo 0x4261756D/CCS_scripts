0549124edd194c4be4addb95c82d899528d54a29
Rundas <38040866+Rundas01@users.noreply.github.com>
2020-12-27 17:14:40 +0100

    "Weirzard Plain" script aktualisiert

--- script/INSU_Extradeck_script/c65000012.lua
+++ script/INSU_Extradeck_script/c65000012.lua
@@ -1,112 +1,117 @@
---Skurrilist Noface
-function c65000012.initial_effect(c)
+--Weirzard Plain
+local s,id=GetID()
+function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c, aux.FilterBoolFunction(Card.IsRace, RACE_SPELLCASTER), 4, 2)
+	Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_SPELLCASTER),4,2)
 	c:EnableReviveLimit()
 	--add
-	local e1 = Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_REMOVE + CATEGORY_TOHAND)
-	e1:SetDescription(aux.Stringid(65000012, 0))
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(c65000012.cost)
-	e1:SetTarget(c65000012.target)
-	e1:SetOperation(c65000012.operation)
-	c:RegisterEffect(e1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
 	--search
-	local e2 = Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(65000012, 1))
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetTarget(c65000012.thtg)
-	e2:SetOperation(c65000012.thop)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 	--cannot remove
-	local e3 = Effect.CreateEffect(c)
+	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_CANNOT_REMOVE)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(0, 1)
-	e3:SetCondition(c65000012.oppremcon)
+	e3:SetTargetRange(1,0)
+	e3:SetCondition(s.remcon)
 	c:RegisterEffect(e3)
-	--opp
-	local e4 = e3:Clone()
-	e4:SetTargetRange(1, 0)
-	e4:SetCondition(c65000012.ownremcon)
+	local e4=e3:Clone()
+	e4:SetTargetRange(0,1)
+	e4:SetCondition(s.remcon2)
 	c:RegisterEffect(e4)
 end
 
-function c65000012.rmfilter(c)
-	return (c:IsType(TYPE_SPELL) and c:IsSetCard(0x800) and c:IsAbleToRemoveAsCost())
+function s.rmfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsSetCard(0x800) and c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(s.thfilter1,tp,LOCATION_REMOVED,0,1,nil,c:GetCode())
 end
-function c65000012.cost(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then return e:GetHandler():CheckRemoveOverlayCard(tp, 1, REASON_COST)
-		and	Duel.IsExistingMatchingCard(c65000012.rmfilter, tp, LOCATION_DECK, 0, 1, nil) end
-	e:GetHandler():RemoveOverlayCard(tp, 1, 1, REASON_COST)
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_REMOVE)
-	local g = Duel.SelectMatchingCard(tp, c65000012.rmfilter, tp, LOCATION_DECK, 0, 1, 1, nil)
-	e:SetLabel(g:GetFirst():GetCode())
-	if g:GetCount() > 0 then
-		Duel.Remove(g, POS_FACEUP, REASON_COST)
-	end
-end
-function c65000012.thfilter1(c,r)
+function s.thfilter1(c,r)
 	return c:IsAbleToHand() and not c:IsCode(r)
 end
-function c65000012.target(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then return Duel.IsExistingMatchingCard(c65000012.thfilter1, tp, LOCATION_REMOVED, 0, 1, nil)
-		and	Duel.GetFieldGroupCount(tp, LOCATION_ONFIELD, LOCATION_ONFIELD)>0 end
---	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
---	local g1 = Duel.SelectMatchingCard(tp, c65000012.thfilter1, tp, tp, LOCATION_REMOVED, 0, 1, nil)
-	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_REMOVED)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
+		and	Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_DECK,0,1,nil) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+	Duel.BreakEffect()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if g then
+		Duel.Remove(g,POS_FACEUP,REASON_COST)
+		e:SetLabel(g:GetCode())
+	end
 end
-function c65000012.operation(e, tp, eg, ep, ev, re, r, rp)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g1 = Duel.SelectMatchingCard(tp, c65000012.thfilter1,tp,LOCATION_REMOVED, 0, 1, 1, nil, e:GetLabel())
-	if g1:GetCount() > 0 then
-		Duel.SendtoHand(g1, nil, REASON_EFFECT)
-	 	Duel.ConfirmCards(1 - tp, g1)
-		Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
-		local g2 = Duel.SelectMatchingCard(tp, Card.IsDestructable, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, 1, nil)
-		if g2:GetCount() > 0 then
-			Duel.Destroy(g2, REASON_EFFECT)
+	local tc=Duel.SelectMatchingCard(tp,s.thfilter1,tp,LOCATION_REMOVED,0,1,1,nil,e:GetLabel()):GetFirst()
+	if tc then
+		if Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_HAND) then
+			Duel.ConfirmCards(1-tp,tc)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e4:SetCode(EFFECT_CANNOT_ACTIVATE)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(1,0)
+			e1:SetTarget(s.sumlimit)
+			e1:SetLabel(tc:GetCode())
+			e4:SetValue(s.aclimit)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e1,tp)
+		end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g2=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g2>0 then
+			Duel.Destroy(g2,REASON_EFFECT)
 		end
 	end
 end
-
-function c65000012.thfilter(c)
+function s.aclimit(e,re)
+	return re:GetHandler():IsCode(e:GetLabel())
+end
+function s.thfilter(c)
 	return c:IsSetCard(0x800) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-function c65000012.thtg(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then return true end
-	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_GRAVE)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
-function c65000012.thop(e, tp, eg, ep, ev, re, r, rp)
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-	local g = Duel.SelectMatchingCard(tp, c65000012.thfilter, tp, LOCATION_GRAVE, 0, 1, 1, nil)
-	if g:GetCount() > 0 then
-		Duel.SendtoHand(g, nil, REASON_EFFECT)
-		Duel.ConfirmCards(1 - tp, g)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
-
-function c65000012.ownremcon(e)
-	if e:GetHandler():IsRace(RACE_SPELLCASTER) then
-		return false
-	else
-		return not Duel.IsExistingMatchingCard(c65000012.remfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
-	end
-end
-
-function c65000012.remfilter(c)
+function s.remfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER)
 end
-
-function c65000012.oppremcon(e)
-	return not Duel.IsExistingMatchingCard(c65000012.remfilter, e:GetHandlerPlayer(), 0, LOCATION_MZONE, 1, nil)
+function s.remcon(e,tp)
+	return not Duel.IsExistingMatchingCard(s.remfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.remcon2(e,tp)
+	return not Duel.IsExistingMatchingCard(s.remfilter,tp,0,LOCATION_MZONE,1,nil)
 end

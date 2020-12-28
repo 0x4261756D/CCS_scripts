cafe282552573263822e1cee6d230d74d05a8d55
Rundas <38040866+Rundas01@users.noreply.github.com>
2020-12-28 11:21:31 +0100

    customutility.lua in den korretken Zustand zurückversetzt
    Für Leute, die was an den Funktionen ändern möchten: bitte so, dass die Funktionsweise nicht geändert wird.
    (Also am Besten nen PR öffnen)

--- script/customutility.lua
+++ script/customutility.lua
@@ -1,38 +1,48 @@
+--Diese Datei enthält alle archetype-unspezifischen Hilfsfunktionen. Jeder Funktion soll eine kleine Beschreibung vorangehen, damit andere auch wissen wie sie verwendet wird.
+
 --Hilfskostenfunktion um eine Anzahl X Xyz Materialien abzuhängen (min=<X=<max). min=nil -> alle Materialien werden abgehängt.
 --label=true -> Die Anzahl abgehängter Materialien wird als Label gespeichert.
 --Es ist ebenfalls möglich eine weitere Kostenfunktion mit einzubinden, indem diese als Parameter 4 mit übergeben wird.
---wird order (Parameter 5) mitgegeben wird cost nach dem Abhängen ausgeführt, sonst davor.
-
+--Es ist darauf zu achten, dass order nicht nil ist wenn cost nicht nil ist.
+--Denn sonst wird die Kostenfunktion gar nicht ausgeführt (order=0 -> die Kostenfunktion wird vor dem Abhängen ausgeführt, order=1 -> danach).
 function Auxiliary.doccost(min,max,label,cost,order)
 	return function(e,tp,eg,ep,ev,re,r,rp,chk)
 		local c=e:GetHandler()
 		local ct,eff,set,label=c:GetOverlayCount(),Duel.IsPlayerAffectedByEffect(tp,CARD_NUMERON_NETWORK),c:IsSetCard(0x14b),label or false
 		local min=min or ct
 		local max=max or min
-		if chk==0 then
-			return (c:CheckRemoveOverlayCard(tp,min,REASON_COST) or (eff and set)) and cost and cost(e,tp,eg,ep,ev,re,r,rp,0)
-		end
-		if cost then
-			if (eff and set) and (ct==0 or (ct>0 and Duel.SelectYesNo(tp,aux.Stringid(CARD_NUMERON_NETWORK,1)))) then
-				cost(e,tp,eg,ep,ev,re,r,rp,1)
-				return true
-			else
-				if order then
-					c:RemoveOverlayCard(tp,min,max,REASON_COST)
-					cost(e,tp,eg,ep,ev,re,r,rp,1)
-				else
-					cost(e,tp,eg,ep,ev,re,r,rp,1)						
-					c:RemoveOverlayCard(tp,min,max,REASON_COST)
+			if chk==0 then 
+				if cost then
+					return (c:CheckRemoveOverlayCard(tp,min,REASON_COST) or (eff and set)) and cost(e,tp,eg,ep,ev,re,r,rp,0)
+					else return c:CheckRemoveOverlayCard(tp,min,REASON_COST) or (eff and set)
 				end
 			end
-		else
-			if (eff and set) and (ct==0 or (ct>0 and Duel.SelectYesNo(tp,aux.Stringid(CARD_NUMERON_NETWORK,1)))) then
-				return true
-			else
-				c:RemoveOverlayCard(tp,min,max,REASON_COST)
+			if cost then
+				if order==0 then
+					if (eff and set) and (ct==0 or (ct>0 and Duel.SelectYesNo(tp,aux.Stringid(CARD_NUMERON_NETWORK,1)))) then
+						cost(e,tp,eg,ep,ev,re,r,rp,1)
+						return true
+							else
+								cost(e,tp,eg,ep,ev,re,r,rp,1)
+								c:RemoveOverlayCard(tp,min,max,REASON_COST)
+					end
+				elseif order==1 then
+					if (eff and set) and (ct==0 or (ct>0 and Duel.SelectYesNo(tp,aux.Stringid(CARD_NUMERON_NETWORK,1)))) then
+						cost(e,tp,eg,ep,ev,re,r,rp,1)
+						return true
+							else
+								c:RemoveOverlayCard(tp,min,max,REASON_COST)
+								cost(e,tp,eg,ep,ev,re,r,rp,1)
+					end
+				else 
+					if (eff and set) and (ct==0 or (ct>0 and Duel.SelectYesNo(tp,aux.Stringid(CARD_NUMERON_NETWORK,1)))) then
+						return true
+							else
+								c:RemoveOverlayCard(tp,min,max,REASON_COST)
+					end
+				end
 			end
-		end
-		if label then 
+		if label==true then 
 			e:SetLabel(#Duel.GetOperatedGroup())
 		end
 	end

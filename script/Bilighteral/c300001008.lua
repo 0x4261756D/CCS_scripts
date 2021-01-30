12296cee29fab4210be8aed64e23e8a15a485a01
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-01-30 14:59:39 +0100

    Update c300001008.lua

--- script/Bilighteral/c300001008.lua
+++ script/Bilighteral/c300001008.lua
@@ -9,6 +9,7 @@ function s.initial_effect(c)
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e0:SetCode(EVENT_CHAIN_SOLVED)
+	e0:SetRange(LOCATION_ALL)
 	e0:SetOperation(s.flagop)
 	c:RegisterEffect(e0)
 	--Activate

84d518ceaa55bc8878caca4f2b963319ab5e6d2c
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-02-05 14:13:55 +0100

    merged the timeleap proc into customutility
    for better accessibility

--- script/customutility.lua
+++ script/customutility.lua
@@ -1,4 +1,10 @@
---This file contains a useful list of functions which can be used for a lot of things. Each function should have a small description as well.
+--This file contains a useful list of functions and constants which can be used for a lot of things. Each function should have a small description as well.
+
+--constants
+SUMMON_TYPE_TIMELEAP=SUMMON_TYPE_LINK+69
+REASON_TIMELEAP=REASON_LINK+69
+SUMMON_TYPE_CHAOS_SYNCHRO=SUMMON_TYPE_SYNCHRO+69
+REASON_CHAOS_SYNCHRO=REASON_SYNCHRO+69
 
 --Function to check whether a card is EXACTLY the passed type (like a more strict version of Card.IsType)
 function Card.CheckType(c,tp)
@@ -110,9 +116,6 @@ Auxiliary.MultiRegister=aux.FunctionWithNamedArgs(
 	e:Reset()
 end,"handler","codes","desc","cat","prop","typ","range","con","cost","tg","op","opt")
 
-SUMMON_TYPE_CHAOS_SYNCHRO=SUMMON_TYPE_SYNCHRO+69
-REASON_CHAOS_SYNCHRO=REASON_SYNCHRO+69
-
 --The following function adds a proc for the Chaos Synchro Summon type to a card (basically Synchro Summoning by banishing materials from the GY with more flexibility).
 --Parameter Explanation:
 --c: The card which receives the proc
@@ -179,5 +182,99 @@ function Auxiliary.ChaosSynchroOperation(c)
 		c:SetMaterial(g)
 		Duel.Remove(g,POS_FACEUP,REASON_MATERIAL+REASON_CHAOS_SYNCHRO)
 		g:DeleteGroup()
+	end
+end
+
+function Card.IsCanBeTimeleapMaterial(c)
+	return c:IsCanBeMaterial(SUMMON_TYPE_TIMELEAP)
+end
+
+--Explanation of params:
+--c=card which will be summoned, con=required conditions, f=filter for materials, min/max=how many materials are needed,
+--desc=optional description, tb=bool to specify whether to time banish in the end phase, extraparams=array containing all extraparams of "f".
+Auxiliary.AddTimeLeapProcedure=aux.FunctionWithNamedArgs(
+	function(c,con,f,min,max,desc,tb,extraparams)
+		local min=min or 1
+		local max=max or min
+		local tb=tb or false
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		if desc then
+			e1:SetDescription(desc)
+			else 
+				e1:SetDescription(3400)
+		end
+		e1:SetCode(EFFECT_SPSUMMON_PROC)
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetRange(LOCATION_EXTRA)
+		e1:SetCondition(aux.TimeLeapCondition(con))
+		e1:SetTarget(aux.TimeLeapTarget(c,f,min,max,table.unpack(extraparams)))
+		e1:SetOperation(aux.TimeLeapOperation(c))
+		e1:SetValue(SUMMON_TYPE_TIMELEAP)
+		c:RegisterEffect(e1)
+			if tb==true then
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+				e2:SetCode(EVENT_PHASE+PHASE_END)
+				e2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+				e2:SetRange(LOCATION_MZONE)
+				e2:SetCondition(function(e) return Duel.GetTurnPlayer()==e:GetHandlerPlayer() end)
+				e2:SetTarget(aux.TimeBanishTarget(c))
+				e2:SetOperation(aux.TimeBanishOperation(c))
+				c:RegisterEffect(e2)
+			end
+end,"handler","con","filter","min","max","desc","time banish","extraparams")
+
+function Auxiliary.TimeLeapCondition(con)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		return (not con or con(e,tp,eg,ep,ev,re,r,rp)) and Duel.GetFlagEffect(tp,3400)==0
+	end
+end
+
+function Auxiliary.TimeLeapTarget(c,f,min,max,...)
+	local params={...}
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		local g=Duel.GetMatchingGroup(Card.IsLevel,tp,LOCATION_MZONE,0,nil,c:GetLevel()-1):Filter(Card.IsCanBeTimeleapMaterial,nil)
+		if #g>=min then
+			local mat=g:FilterSelect(tp,f,min,max,true,nil,table.unpack(params))
+			e:SetLabelObject(mat)
+			return true
+			else return false
+		end
+	end
+end
+
+function Auxiliary.TimeLeapOperation(c)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		local mat=e:GetLabelObject()
+		c:SetMaterial(mat)
+		Duel.Remove(mat,POS_FACEUP,REASON_MATERIAL+REASON_FUSION+69)
+		Duel.RegisterFlagEffect(tp,3400,RESET_PHASE+PHASE_END,EFFECT_FLAG_OATH,1)
+		mat:RegisterFlagEffect(3401,RESET_PHASE+PHASE_END,0,1,EFFECT_FLAG_CLIENT_HINT,1,0,3401)
+	end
+end
+
+function Auxiliary.TimeBanishTarget(c)
+	return function(e,tp,eg,ep,ev,re,r,rp,chk)
+		if chk==0 then return c:IsAbleToRemove() end
+	end
+end
+
+function Auxiliary.TimeBanishFilter(c)
+	return c:GetFlagEffect(3401)~=0
+end
+
+function Auxiliary.TimeBanishOperation(c)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		Duel.Remove(c,POS_FACEUP,REASON_EFFECT,tp)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_REMOVED)
+		e1:SetValue(1)
+		c:RegisterEffect(e1,true)
+		local tc=Duel.SelectMatchingCard(tp,aux.spfilter(e,tp,0,aux.TimeBanishFilter),tp,LOCATION_REMOVED,0,1,1,nil)
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

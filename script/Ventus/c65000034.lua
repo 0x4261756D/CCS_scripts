d2fa4f0c93c3b132deb7422575f7ab708e09dc14
0x4261756D <nebbenv.google@bs4a.de>
2021-05-01 01:00:26 +0200

    removed Debug messages

--- script/Ventus/c65000034.lua
+++ script/Ventus/c65000034.lua
@@ -65,8 +65,6 @@ function s.spoperation(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	Debug.Message(rp)
-	Debug.Message(e:GetHandlerPlayer())
 	if rp==e:GetHandlerPlayer() then return end
     local rc=nil
 	if r==REASON_BATTLE+REASON_DESTROY then

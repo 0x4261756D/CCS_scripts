02629f4e4ec93666c215cd511854614e7bb81682
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-05-01 23:48:19 +0200

    Update c65000026.lua

--- script/Ventus/c65000026.lua
+++ script/Ventus/c65000026.lua
@@ -1,5 +1,5 @@
 --Ventus Zera
-local s, id = GetID()
+local s,id=GetID()
 function s.initial_effect(c)
 	--atk
 	local e1=Effect.CreateEffect(c)
@@ -14,9 +14,9 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TODECK)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_TO_DECK)
-	e2:SetCountLimit(1,65000026)
+	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.spcon)
 	e2:SetValue(1)
 	e2:SetTarget(s.sptg)

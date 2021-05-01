6f9171691c0ff9bafd761859ba82af5a4c3acb3d
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-05-01 09:51:47 +0200

    Update c64000161.lua

--- script/INSU_Maindeck_script/c64000161.lua
+++ script/INSU_Maindeck_script/c64000161.lua
@@ -1,4 +1,5 @@
-local s, id = GetID()
+--Injection Phobia Reactor
+local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -7,10 +8,10 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetCountLimit(1,64000161)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.con)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
-	e1:SetCondition(s.con)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--act in hand
@@ -18,33 +19,32 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
 	e2:SetCondition(s.condition)
-	e2:SetCountLimit(1,64000161)
+	e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
 	--add
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(64000161,0))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_TOHAND)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_DESTROYED)
 	e3:SetRange(LOCATION_GRAVE)
-	e3:SetCountLimit(1,64000161)
-	e3:SetCost(aux.bfgcost)
+	e3:SetCountLimit(1,id)
 	e3:SetCondition(s.spcon)
+	e3:SetCost(aux.bfgcost)
 	e3:SetTarget(s.tar)
 	e3:SetOperation(s.act)
 	c:RegisterEffect(e3)
 end
-
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,500) end
 	Duel.PayLPCost(tp,500)
 end
-function s.con(e)
-	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_MZONE)>0
+function s.con(e,tp)
+	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
 end
 function s.filter(c,e,tp)
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (c:IsCode(79575620) or c:IsCode(5519829) or c:IsSetCard(0x19d)) and c:IsType(TYPE_MONSTER)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and (c:IsCode(79575620,5519829) or c:IsSetCard(0x19d)) and c:IsType(TYPE_MONSTER)
 end
 function s.filter1(c)
 	return c:IsFaceup() and

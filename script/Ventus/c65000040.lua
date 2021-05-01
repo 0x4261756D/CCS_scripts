57e80904a249bd1e9e597cf3f2154df2a59706bb
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-05-01 23:32:31 +0200

    Update c65000040.lua

--- script/Ventus/c65000040.lua
+++ script/Ventus/c65000040.lua
@@ -1,10 +1,10 @@
-local s, id = GetID()
+--Inner Conflict of Ventus
+local s,id=GetID()
 function s.initial_effect(c)
 --Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
 	--negate attack

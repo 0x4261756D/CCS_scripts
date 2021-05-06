5a42c737e5cc4137df25485a809d2678a5f70049
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-05-06 11:09:12 +0200

    Update c63000025.lua

--- script/Ojama/c63000025.lua
+++ script/Ojama/c63000025.lua
@@ -23,16 +23,15 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) then return end
+	if not e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) or Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
 	if e:GetHandler():IsRelateToEffect(e) then
 		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-
 function s.cfilter(c)
 	return c:IsSetCard(0xf) and not c:IsPublic()
 end
@@ -54,5 +53,3 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Draw(p,ct+1,REASON_EFFECT)
 	end
 end
-
-

8e92ad54c99d196839dec92ef69fea57f33247de
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-05-07 23:40:06 +0200

    Update bilightutility.lua

--- script/Bilighteral/bilightutility.lua
+++ script/Bilighteral/bilightutility.lua
@@ -98,3 +98,48 @@ function Bilighteral.bmcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.SelectMatchingCard(tp,Bilighteral.remfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(tc,POS_FACEUP,REASON_COST)
 end
+
+local regeff2=Card.RegisterEffect
+function Card.RegisterEffect(c,e,forced,...)
+	if c:IsStatus(STATUS_INITIALIZING) and not e then
+		error("Parameter 2 expected to be Effect, got nil instead.",2)
+	end
+	--1 == 511002571 - access to effects that activate that detach an Xyz Material as cost
+	--2 == 511001692 - access to Cardian Summoning conditions/effects
+	--4 ==  12081875 - access to Thunder Dragon effects that activate by discarding
+	--8 == 511310036 - access to Allure Queen effects that activate by sending themselves to GY
+	--16 == 300001010 - access to Effulgence Congregater Zalatiel to use hopts twice
+	local reg_e=regeff2(c,e,forced)
+	if not reg_e then
+		return nil
+	end
+	local reg={...}
+	local resetflag,resetcount=e:GetReset()
+	for _,val in ipairs(reg) do
+		local prop=EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE
+		if e:IsHasProperty(EFFECT_FLAG_UNCOPYABLE) then prop=prop|EFFECT_FLAG_UNCOPYABLE end
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(prop,EFFECT_FLAG2_MAJESTIC_MUST_COPY)
+		if val==1 then
+			e2:SetCode(511002571)
+		elseif val==2 then
+			e2:SetCode(511001692)
+		elseif val==4 then
+			e2:SetCode(12081875)
+		elseif val==8 then
+			e2:SetCode(511310036)
+		elseif val==16 then
+			e2:SetCode(300001010)
+		end
+		e2:SetLabelObject(e)
+		e2:SetLabel(c:GetOriginalCode())
+		if resetflag and resetcount then
+			e2:SetReset(resetflag,resetcount)
+		elseif resetflag then
+			e2:SetReset(resetflag)
+		end
+		c:RegisterEffect(e2)
+	end
+	return reg_e
+end

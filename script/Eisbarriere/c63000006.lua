99b712bc877201b96e0515b9d90d5fd6648a6442
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-05-07 20:36:25 +0200

    Update c63000006.lua

--- script/Eisbarriere/c63000006.lua
+++ script/Eisbarriere/c63000006.lua
@@ -1,9 +1,9 @@
 --Leitwolf of the Ice Barrier
+Duel.LoadScript("customutility.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-Duel.LoadScript("customutility.lua")
 local limits = getFreeIdSpace(3)
-	--special summon
+	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -12,11 +12,12 @@ local limits = getFreeIdSpace(3)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(s.spcon)
 	c:RegisterEffect(e1)
+	--To hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetCountLimit(1,limits + 1)
 	e2:SetTarget(s.tg)
@@ -28,26 +29,19 @@ local limits = getFreeIdSpace(3)
 	local e4=e2:Clone()
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e4)
+	--Search
 	local e5=Effect.CreateEffect(c)
 	e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e5:SetType(EFFECT_TYPE_ACTIVATE)
-	e5:SetCode(EVENT_FREE_CHAIN)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetCode(EVENT_LEAVE_FIELD)
 	e5:SetCountLimit(1,limits + 2)
 	e5:SetCondition(s.descon)
 	e5:SetTarget(s.target)
 	e5:SetOperation(s.activate)
 	c:RegisterEffect(e5)
-	local e6=e5:Clone()
-	e6:SetCode(EVENT_REMOVE)
-	c:RegisterEffect(e6)
-	local e7=e5:Clone()
-	e7:SetCode(EVENT_TO_DECK)
-	c:RegisterEffect(e7)
 	end
 function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0X2F) and c:IsLevelAbove(4)
+	return c:IsFaceup() and c:IsSetCard(0X2f) and c:IsLevelAbove(4)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -56,15 +50,15 @@ function s.spcon(e,c)
 end
 
 function s.cfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0X2F) and c:IsAbleToHand()
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0X2f) and c:IsAbleToHand()
 end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,2,nil) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local sg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
-	if sg:GetCount()<2 then return end
+	if #sg<2 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=sg:Select(tp,2,2,nil)
 	Duel.SendtoHand(g,nil,REASON_EFFECT)
@@ -74,16 +68,16 @@ function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousPosition(POS_FACEUP) and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.zfilter(c)
-	return c:IsLevelBelow(4) and c:IsSetCard(0X2F) and c:IsAbleToHand()
+	return c:IsLevelBelow(4) and c:IsSetCard(0X2f) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.zfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.zfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
+	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end

061e5b0c72d9b031aeab79b32ec687f2060841f6
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-05-08 11:31:55 +0200

    Update c64000161.lua

--- script/INSU_Maindeck_script/c64000161.lua
+++ script/INSU_Maindeck_script/c64000161.lua
@@ -3,8 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE+CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
@@ -50,43 +49,43 @@ function s.filter1(c)
 	return c:IsFaceup() and
 		c:IsCanChangePosition() and
 		c:IsType(TYPE_EFFECT) and 
-		not c:IsDisabled()
+		not c:IsDisabled() and c:IsAttackPos()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_DECK) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 			and Duel.IsExistingMatchingCard(s.filter1,tp,0,LOCATION_MZONE,1,nil)
 	end
-	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE+CATEGORY_POSITION,nil,1,1-tp,LOCATION_MZONE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c = e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-	local a = Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	if #a > 0 then
-		Duel.SpecialSummon(a, 0, tp, tp, false, false, POS_FACEUP)
-		if c:IsRelateToEffect(e) then
-			local g=Duel.SelectMatchingCard(tp,s.filter1,tp,0,LOCATION_MZONE,1,1,nil)
-			if g:GetCount()>0 then
-				local tc1=g:GetFirst()
-				Duel.ChangePosition(tc1,POS_FACEUP_DEFENSE)
-				local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	if not Duel.IsExistingMatchingCard(s.filter1,tp,0,LOCATION_MZONE,1,nil) or not Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		or Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local a=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #a>0 then
+		if c:IsRelateToEffect(e) and Duel.SpecialSummon(a,0,tp,tp,false,false,POS_FACEUP)>0 then
+			local tc=Duel.SelectMatchingCard(tp,s.filter1,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+			if tc then
+				Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
+				local e1=Effect.CreateEffect(c)
 				e1:SetType(EFFECT_TYPE_SINGLE)
 				e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-				e1:SetReset(RESET_EVENT+0x1fe0000)
-				tc1:RegisterEffect(e1)
-				local e2=Effect.CreateEffect(e:GetHandler())
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e1)
+				local e2=Effect.CreateEffect(c)
 				e2:SetType(EFFECT_TYPE_SINGLE)
 				e2:SetCode(EFFECT_DISABLE)
-				e2:SetReset(RESET_EVENT+0x1fe0000)
-				tc1:RegisterEffect(e2)
-				local e3=Effect.CreateEffect(e:GetHandler())
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e2)
+				local e3=Effect.CreateEffect(c)
 				e3:SetType(EFFECT_TYPE_SINGLE)
 				e3:SetCode(EFFECT_DISABLE_EFFECT)
 				e3:SetValue(RESET_TURN_SET)
-				e3:SetReset(RESET_EVENT+0x1fe0000)
-				tc1:RegisterEffect(e3)
+				e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e3)
 			end
 		end
 	end

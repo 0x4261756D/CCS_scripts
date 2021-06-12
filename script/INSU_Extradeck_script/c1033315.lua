b0c554d2f162ce7c233ca4f68d2f6f3a3814eec6
0x4261756D <nebbenv.google@bs4a.de>
2021-06-12 02:28:26 +0200

    fixed Injection Mina (not including Injection Archetype, code cleanup) fixed Injection Fun (confusion with filters...)

--- script/INSU_Extradeck_script/c1033315.lua
+++ script/INSU_Extradeck_script/c1033315.lua
@@ -1,80 +1,78 @@
 --Injection Mina
 --Aubertin
-function c1033315.initial_effect(c)
+local s, id = GetID()
+function s.initial_effect(c)
 	--link summon
-	Link.AddProcedure(c,nil,2,2,c1033315.lcheck)
+	Link.AddProcedure(c,nil,2,2,s.lcheck)
 	c:EnableReviveLimit()
 	--search
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(1033315,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCountLimit(1,1033315)
-	e1:SetTarget(c1033315.tg)
-	e1:SetOperation(c1033315.op)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	local e3=e1:Clone()
-	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	c:RegisterEffect(e3)
 	--special
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(1033315,1))
+	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
 	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_BATTLE_DAMAGE)
-	e4:SetCountLimit(1,1033316)
-	e4:SetCost(c1033315.cost)
-	e4:SetCondition(c1033315.condition)
-	e4:SetTarget(c1033315.target)
-	e4:SetOperation(c1033315.operation)
+	e4:SetCountLimit(1,id + 1)
+	e4:SetCost(s.cost)
+	e4:SetCondition(s.condition)
+	e4:SetTarget(s.target)
+	e4:SetOperation(s.operation)
 	c:RegisterEffect(e4)
 end
-function c1033315.matfilter(c)
+function s.matfilter(c)
 	return c:IsRace(RACE_SPELLCASTER) and c:IsAttribute(ATTRIBUTE_EARTH)
 end
-function c1033315.lcheck(g,lc)
-	return g:IsExists(c1033315.matfilter,1,nil)
+function s.lcheck(g,lc)
+	return g:IsExists(s.matfilter,1,nil)
 end
-function c1033315.filter(c)
-	return c:IsSetCard(0x19d) and c:IsAbleToHand()
+function s.filter(c)
+	return (c:IsSetCard(0x19e) or c:IsSetCard(0x19d) or c:IsCode(79575620)) and c:IsAbleToHand()
 end
-function c1033315.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c1033315.filter,tp,LOCATION_GRAVE,0,1,nil) end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
-function c1033315.op(e,tp,eg,ep,ev,re,r,rp)
+function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c1033315.filter),tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c1033315.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,1000) end
 	Duel.PayLPCost(tp,1000)
 end
-function c1033315.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return ep~=tp
 end
-function c1033315.filterf(c,e,tp)
+function s.filterf(c,e,tp)
 	return c:IsCode(5519829) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c1033315.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c1033315.filterf,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.filterf,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
 end
-function c1033315.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c1033315.filterf,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.filterf,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		Duel.Recover(tp,500,REASON_EFFECT)
--- script/INSU_Maindeck_script/c64000155.lua
+++ script/INSU_Maindeck_script/c64000155.lua
@@ -8,23 +8,23 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
-	e1:SetCountLimit(1,64000155)
+	e1:SetCountLimit(1, id)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c)
+function s.filter(c)
 	return c:IsFaceup() and (c:IsCode(79575620) or c:IsCode(5519829))
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingMatchingCard(s.filter2, tp, LOCATION_DECK, 0, 1, nil)
 end
 function s.filter2(c)
-	return (c:IsSetCard(0x19d) or c:IsSetCard(0x19e) or c:IsCode(79575620))  and c:IsAbleToHand() and not c:IsCode(id)
+	return (c:IsSetCard(0x19d) or c:IsSetCard(0x19e) or c:IsCode(79575620)) and c:IsAbleToHand() and not c:IsCode(id)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:GetControler()==tp and chkc:GetLocation()==LOCATION_DECK and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)

7609dc2e3e15fd4d66709992b26e9fbad649efdc
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-07-08 14:33:48 +0200

    Fix + shorten script of "Errata Representative"

--- script/INSU_Maindeck_script/c10304899.lua
+++ script/INSU_Maindeck_script/c10304899.lua
@@ -1,12 +1,12 @@
 --Errata Representative
-local s, id = GetID()
+Duel.LoadScript("customutility.lua")
+local s,id=GetID()
 function s.initial_effect(c)
 	--change effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_MZONE)
-	--e1:SetCountLimit(1)
 	e1:SetCondition(s.chcon)
 	e1:SetCost(s.chcost)
 	e1:SetOperation(s.chop)
@@ -20,37 +20,32 @@ function s.chcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
 end
 function s.chop(e,tp,eg,ep,ev,re,r,rp)
-	local che1=Duel.IsExistingMatchingCard(aux.TRUE,rp,0,LOCATION_ONFIELD,1,nil)
-	local a1=nil
-	if che1==false then a1=1
-	else a1=Duel.SelectOption(1-tp,aux.Stringid(10304899,0),aux.Stringid(10304899,1))
-	end
-	if a1==1 then
-		Debug.Message(tp)
-		Duel.Hint(HINT_OPSELECTED,tp,e:GetDescription())
-		local g=Group.CreateGroup()
-		--Debug.Message("Destruction Operation selected")
-		Duel.ChangeTargetCard(ev,g)
-		Duel.ChangeChainOperation(ev,s.repop1)
-	elseif a1==0 then
-		Duel.Hint(HINT_OPSELECTED,tp,e:GetDescription())
-		--Debug.Message("Attack change operation selected")
-		local g=Group.CreateGroup()
-		Duel.ChangeTargetCard(ev,g)
-		Duel.ChangeChainOperation(ev,s.repop2)
-	end
+	local cons,strings,ops={Duel.IsExistingMatchingCard(aux.TRUE,rp,LOCATION_ONFIELD,0,1,nil),Duel.IsExistingMatchingCard(Card.IsFaceup,rp,0,LOCATION_MZONE,1,nil)},{aux.Stringid(id,1),aux.Stringid(id,0)},{
+		function(e,tp,eg,ep,ev,re,r,rp)
+			local g=Group.CreateGroup()
+			Duel.ChangeTargetCard(ev,g)
+			Duel.ChangeChainOperation(ev,s.repop1)
+		end,
+		function(e,tp,eg,ep,ev,re,r,rp) 
+			local g=Group.CreateGroup()
+			Duel.ChangeTargetCard(ev,g)
+			Duel.ChangeChainOperation(ev,s.repop2)
+		end}
+	aux.EffectCheck(1-rp,cons,strings,ops)(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.repop1(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,0,1,1,nil)
-	if g:GetCount()>0 then
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,0,1,1,nil)
+	if #g>0 then
 		Duel.HintSelection(g)
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
 function s.repop2(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()    
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
 	local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-	if g:GetCount()>0 then
+	if #g>0 then
 		Duel.HintSelection(g)
 		local tc=g:GetFirst()
 		local e1=Effect.CreateEffect(c)

787b54ecac342b89d100adc918ab4c84870110fa
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-07-28 22:43:47 +0200

    Added "Primathmech Hamiltonian"

---
+++ script/Mathmech/c88021908.lua
@@ -0,0 +1,82 @@
+--Primathmech Hamiltonian
+Duel.LoadScript("summontypes.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x132),4,2,nil,nil,7)
+	--Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(aux.SumtypeCon(c,SUMMON_TYPE_XYZ))
+	e1:SetCost(s.thcost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Synchro Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(s.syncost)
+	e2:SetTarget(s.syntg)
+	e2:SetOperation(s.synop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x132}
+--Search
+function s.tdfilter(c,tp)
+	return c:IsAbleToDeckAsCost() and c:IsSetCard(0x132) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,c:GetCode())
+end
+function s.thfilter(c,code)
+	return c:IsAbleToHand() and c:IsSetCard(0x132) and not c:IsCode(code)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_GRAVE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local td=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
+	e:SetLabel(td:GetCode())
+	Duel.SendtoDeck(td,nil,SEQ_DECKSHUFFLE,REASON_COST)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local th=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e:GetLabel())
+	Duel.SendtoHand(th,tp,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,th)
+end
+--Shinkuro Shokan
+function s.rescon(sg,e,tp,mg)
+	return Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil,sg,e,tp)
+end
+function s.synfilter(c,mat,e,tp)
+	return c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false) and c:IsSynchroSummonable(nil,mat)
+end
+function s.syncost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=e:GetHandler():GetOverlayGroup()
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,99,s.rescon,0) end
+	local mat=aux.SelectUnselectGroup(g,e,tp,2,99,s.rescon,1,tp,HINTMSG_MATERIAL,s.rescon,nil,false)
+	mat:KeepAlive()
+	e:SetLabelObject(mat)
+	Duel.SendtoGrave(mat,REASON_COST)
+end
+function s.syntg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.synop(e,tp,eg,ep,ev,re,r,rp)
+	local mat=e:GetLabelObject()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,mat,e,tp):GetFirst()
+	Duel.SpecialSummon(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
+	sc:CompleteProcedure()
+	sc:SetMaterial(mat)
+	mat:DeleteGroup()
+end

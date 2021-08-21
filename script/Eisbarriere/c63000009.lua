03f00faf98a7a44c670802250a092ad99b75c1a2
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-08-21 17:35:55 +0200

    Update c63000009.lua

--- script/Eisbarriere/c63000009.lua
+++ script/Eisbarriere/c63000009.lua
@@ -15,34 +15,33 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_MZONE,0,1,nil,0X2F) and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1
 end
 function s.filter(c,e,tp)
-	return c:IsCode(9056100) or c:IsCode(53921056) or c:IsCode(81825063)or c:IsCode(81275309) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsCode(9056100,53921056,81825063,81275309) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
-	if g:GetCount()>0 then
-	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(s.indtg)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetValue(1)
-	Duel.RegisterEffect(e1,tp)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	Duel.RegisterEffect(e2,tp)
-	local e3=e1:Clone()
-	e2:SetCode(EFFECT_CANNOT_REMOVE)
-	Duel.RegisterEffect(e3,tp)
-end
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetTarget(s.indtg)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		e1:SetValue(1)
+		Duel.RegisterEffect(e1,tp)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		Duel.RegisterEffect(e2,tp)
+		local e3=e1:Clone()
+		e2:SetCode(EFFECT_CANNOT_REMOVE)
+		Duel.RegisterEffect(e3,tp)
+	end
 end
 function s.indtg(e,c)
 	return c:IsSetCard(0X2F)

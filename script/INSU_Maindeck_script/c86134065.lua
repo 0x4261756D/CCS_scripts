c510bf9be3355c9889bc04505c3d2deb764b9458
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-08-21 14:54:14 +0200

    Update c86134065.lua

--- script/INSU_Maindeck_script/c86134065.lua
+++ script/INSU_Maindeck_script/c86134065.lua
@@ -1,5 +1,5 @@
---dark scorpion memorial
-local s, id = GetID()
+--Dark Scorpion Memorial
+local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -34,43 +34,37 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 	end
 function s.filter(c,e,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x1a) or c:IsCode(76922029) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsType(TYPE_MONSTER) and (c:IsSetCard(0x1a) or c:IsCode(76922029)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return true end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	if g:GetCount()>0 then
+	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-	function s.limit(e,c,sump,sumtype,sumpos,targetp,se)
+function s.limit(e,c,sump,sumtype,sumpos,targetp,se)
 	return c:IsLevelAbove(5) or c:IsRankAbove(5)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and eg:GetFirst():IsControler(tp) and eg:GetFirst():IsSetCard(0x1a) or eg:GetFirst():IsCode(76922029)
+	return ep~=tp and eg:GetFirst():IsControler(tp) and (eg:GetFirst():IsSetCard(0x1a) or eg:GetFirst():IsCode(76922029))
 end
-	function s.filter2(c,e,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x1a) or c:IsCode(76922029) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:GetLocation()==LOCATION_GRAVE and chkc:GetControler()==tp
-		and s.filter(chkc, e, tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chkc then return chkc:GetLocation()==LOCATION_GRAVE and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local g = Duel.GetFirstTarget()
-	if g then
+	local g=Duel.GetFirstTarget()
+	if g and g:IsRelateToEffect(e) then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

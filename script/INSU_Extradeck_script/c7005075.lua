fa363c96c547335831fbcc321433368cae948c6f
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-01-25 22:54:19 +0100

    Fix Tzolkin

--- script/INSU_Extradeck_script/c7005075.lua
+++ script/INSU_Extradeck_script/c7005075.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	Link.AddProcedure(c,s.filter,2,nil,s.spcheck)
 	--2grave
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	--e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,{id, 1})
@@ -37,7 +37,7 @@ function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	return bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_LINK)==SUMMON_TYPE_LINK
 end
 function s.filter2(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGrave()
+	return c:IsSpellTrap() and c:IsAbleToGrave()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
@@ -58,7 +58,7 @@ function s.seqcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.seqcfilter,1,nil,tp,lg)
 end
 function s.setfilter(c)
-	return (c:GetType()==TYPE_TRAP or c:GetType()==TYPE_SPELL) and c:IsSSetable()
+	return c:IsSpellTrap() and c:IsSSetable()
 end
 function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.setfilter(chkc) end
@@ -73,5 +73,5 @@ function s.seqop(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
 		Duel.SSet(tp,tc)
 		Duel.ConfirmCards(1-tp,tc)
-		end
-		end
+	end
+end

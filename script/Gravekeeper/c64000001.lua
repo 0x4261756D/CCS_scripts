32ac75d8f7ae2a0abfd82d7b67ce0dffd92fe2ee
Coroln <76746930+Coroln@users.noreply.github.com>
2023-04-18 21:10:09 +0200

    Added Qli and Gravekeeper's (#41)
    * Added Qli and Gravekeeper's
    
    Added Qli Support
    -Apoqliphort Binary
    Added Gravekeeper's Support
    -Gravekeeper's Scholar
    -Gravekeeper's Priest
    -Gravekeeper's Last Remains of Anubis
    
    * Update c52894686.lua
    
    * Update c64000001.lua
    
    * Update c64000002.lua
    
    * Update c64000004.lua
    
    * Update c52894685.lua
    
    ---------
    
    Co-authored-by: Rundas <38040866+Rundas01@users.noreply.github.com>

---
+++ script/Gravekeeper/c64000001.lua
@@ -0,0 +1,66 @@
+--Gravekeeper's Scholar
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special summon itself from hand
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e0:SetType(EFFECT_TYPE_IGNITION)
+	e0:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e0:SetCountLimit(1,id)
+	e0:SetCondition(s.spcon)
+	e0:SetCost(s.spcost)
+	e0:SetTarget(s.sptg)
+	e0:SetOperation(s.spop)
+	c:RegisterEffect(e0)
+	--boost
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetCondition(s.necrocon)
+	e1:SetTarget(s.atktg)
+	e1:SetValue(500)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x2E}
+s.listed_names={CARD_NECROVALLEY}
+--Special summon itself from hand
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x2E) and c:IsMonster()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.cfilter(c)
+	return c:IsAbleToGraveAsCost()
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) 
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		 Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
+--boost
+function s.necrocon(e)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_NECROVALLEY),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+		or Duel.IsEnvironment(CARD_NECROVALLEY)
+end
+function s.atktg(e,c)
+	return c:IsSetCard(0x2E)
+end

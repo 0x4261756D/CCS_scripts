7a258b162affbc2f5c02aece9ad068b866dfe4b2
Coroln <76746930+Coroln@users.noreply.github.com>
2024-08-11 10:26:46 +0200

    Iron Chain Support
    -Iron Chain Minotaur
    -Iron Chain Hound
    -Millennium Chain
    -Iron Chain Ring (Made by NIOIN)

---
+++ script/Iron Chain/c30000008.lua
@@ -0,0 +1,54 @@
+--Iron Chain Ring
+--Script by NIOIN
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate (add to hand)
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+    --mill
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCategory(CATEGORY_DECKDES)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCondition(s.condtion)
+    e2:SetTarget(s.target)
+    e2:SetOperation(s.operation)
+    c:RegisterEffect(e2)
+end
+--mill
+function s.cfilter(c,tp)
+    return c:IsPreviousLocation(LOCATION_DECK) and c:IsPreviousControler(tp)
+end
+function s.condtion(e,tp,eg,ep,ev,re,r,rp)
+    if not re then return false end
+    local rc=re:GetHandler()
+    return re:GetHandler():GetCode()~=id and (r&REASON_EFFECT)~=0 and eg:IsExists(s.cfilter,1,nil,1-tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsRelateToEffect(e) end
+    Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,1-tp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    Duel.DiscardDeck(1-tp,1,REASON_EFFECT)
+end
+--add to hand
+function s.filter(c)
+    return c:IsSetCard(0x25) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+    if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local sg=g:Select(tp,1,1,nil)
+        Duel.SendtoHand(sg,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,sg)
+    end
+end
---
+++ script/Iron Chain/c98574213.lua
@@ -0,0 +1,51 @@
+--Iron Chain Minotaur
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+    --special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={id}
+--special summon
+function s.dspfilter(c,e,tp)
+	return c:IsSetCard(0x25) and c:IsLevelBelow(4) and not c:IsCode(id)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+    --Cannot Special Summon from the Extra Deck, except Synchro monsters
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	--Clock Lizard check
+	aux.addTempLizardCheck(e:GetHandler(),tp,function(_,c) return not c:IsOriginalType(TYPE_SYNCHRO) end)
+    --
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,s.dspfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+		if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+            Duel.DiscardDeck(1-tp,2,REASON_EFFECT)
+		end
+	end
+	Duel.SpecialSummonComplete()
+end

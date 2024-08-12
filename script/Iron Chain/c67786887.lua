284ad8d9392fe175262319f45407f91c3d09750e
Coroln <76746930+Coroln@users.noreply.github.com>
2024-08-12 03:31:13 +0200

    Iron Chain Support (#52)
    -Iron Chain Minotaur
    -Iron Chain Hound
    -Millennium Chain
    -Iron Chain Ring (Made by NIOIN)

---
+++ script/Iron Chain/c67786887.lua
@@ -0,0 +1,44 @@
+--Iron Chain Hound
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 1 "Iron Chain" monster from the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_TELLAR)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x25}
+s.listed_names={id}
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x25) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,1-tp,1)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 and g:GetFirst():IsLocation(LOCATION_HAND) then
+		local ct=g:GetFirst():GetLevel()
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		Duel.DiscardDeck(1-tp,ct,REASON_EFFECT)
+	end
+end

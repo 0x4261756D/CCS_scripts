29153422597b4650848a1c828f0b09769b75f3d5
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2024-08-12 20:25:02 +0200

    Fix indentation and typos

--- script/Elemental Floater/c83858954.lua
+++ script/Elemental Floater/c83858954.lua
@@ -2,28 +2,28 @@
 --Script by Coroln
 local s,id=GetID()
 function s.initial_effect(c)
-    --special summon
+	--special summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_CAL)
 	e2:SetCode(EVENT_BATTLE_DAMAGE)
-    e2:SetRange(LOCATION_MZONE)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetCondition(s.spcon2)
 	e2:SetTarget(s.sptg2)
 	e2:SetOperation(s.spop2)
 	c:RegisterEffect(e2)
 end
 function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-	return ep==tp and c:IsControler(tp) and e:GetHandler():IsRelateToBattle()
+	local c=e:GetHandler()
+	return ep==tp and c:IsControler(tp) and c:IsRelateToBattle()
 end
 function s.filter(c,e,tp,dam)
 	return (c:IsRace(RACE_PYRO) or c:IsAttribute(ATTRIBUTE_FIRE)) and c:IsAttackBelow(dam) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp,ev) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end

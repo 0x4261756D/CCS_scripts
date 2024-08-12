89aafa8294c8a1e5d10be88663e77e9e8b91ddc6
Coroln <76746930+Coroln@users.noreply.github.com>
2024-08-12 20:27:21 +0200

    Elemental Floater (#53)
    * Added Floater that Special Via Damage
    
    Co-authored-by: 0x4261756D <38735823+0x4261756D@users.noreply.github.com>

---
+++ script/Elemental Floater/c56843848.lua
@@ -0,0 +1,37 @@
+--Golden Chimera
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.spcon2)
+	e2:SetTarget(s.sptg2)
+	e2:SetOperation(s.spop2)
+	c:RegisterEffect(e2)
+end
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return ep==tp and c:IsControler(tp) and c:IsRelateToBattle()
+end
+function s.filter(c,e,tp,dam)
+	return (c:IsRace(RACE_WINGEDBEAST) or c:IsAttribute(ATTRIBUTE_WIND)) and c:IsAttackBelow(dam) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp,ev) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ev)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

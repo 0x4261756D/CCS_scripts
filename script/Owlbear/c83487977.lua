fe65e7503642321ccd32da626b36728d043b887a
Coroln <76746930+Coroln@users.noreply.github.com>
2025-03-26 16:55:55 +0100

    Added "Owlbear"
    If you control a Tuner Monster, you can Special Summon this card (from your hand). A non-Effect monster that was Synchro Summoned using this card on the field as Material gains this effect.
    ●Once per turn: You can send 1 EARTH Beast monster from your Deck to the GY; Destroy 1 monster your opponent controls.

---
+++ script/Owlbear/c83487977.lua
@@ -0,0 +1,82 @@
+--Owlbear
+--Script by Coroln
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+    --effect gain
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCondition(s.efcon)
+	e2:SetOperation(s.efop)
+	c:RegisterEffect(e2)
+end
+--special summon
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_TUNER)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+--effect gain
+function s.efcon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_SYNCHRO)~=0
+		and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.efop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+    --send to GY and destroy
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+    e1:SetCost(s.tdcost)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
+	if not rc:IsType(TYPE_EFFECT) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_ADD_TYPE)
+		e2:SetValue(TYPE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		rc:RegisterEffect(e2,true)
+	end
+end
+--send to GY and destroy
+function s.cosfilter(c)
+    return c:IsMonster() and c:IsRace(RACE_BEAST) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAbleToGraveAsCost()
+end
+function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cosfilter,tp,LOCATION_DECK,0,1,nil) end
+    local g1=Duel.SelectMatchingCard(tp,s.cosfilter,tp,LOCATION_DECK,0,1,1,nil)
+    Duel.SendtoGrave(g1,REASON_COST)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsMonster,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,Card.IsMonster,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end

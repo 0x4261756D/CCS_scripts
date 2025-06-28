66c2a588682d98584e585849b2b35556d4235bc3
Rika <113550549+RikaCPP@users.noreply.github.com>
2025-06-28 21:11:43 +0200

    Bug fix (#56)
    Floaters didn´t work correctly and triggered incorectly. They now trigger as intended.

--- script/INSU_Maindeck_script/c47561239.lua
+++ script/INSU_Maindeck_script/c47561239.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetCondition(s.condition1)
 	e1:SetTarget(s.target1)
@@ -24,7 +24,8 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-		return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
+    c = e:GetHandler()
+    return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_DESTROY) and (c:IsReason(REASON_EFFECT) or c:IsReason(REASON_BATTLE))
 end
 function s.filter(c,e,tp)
 	return c:IsAttackBelow(1500) and c:IsAttribute(ATTRIBUTE_FIRE)
